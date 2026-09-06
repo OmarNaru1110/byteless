50b59345800d47a5e89a6fa2d34bf71d9bdf7aec
Naru <omarnaru2002@gmail.com>
2026-09-06 15:03:49 +0300

    feat: add logs to ffprobe builder

--- internal/builder/ffprobe_builder.go
+++ internal/builder/ffprobe_builder.go
@@ -1,5 +1,7 @@
 package builder
 
+import "log"
+
 type FfprobeOptions struct {
 	logLevel      string
 	showEntries   string
@@ -20,26 +22,31 @@ func NewFfprobeBuilder(ffprobePath string) *FfprobeBuilder {
 
 func (b *FfprobeBuilder) SetLogLevel(logLevel string) *FfprobeBuilder {
 	b.options.logLevel = logLevel
+	log.Printf("FfprobeBuilder: log level set to %q", logLevel)
 	return b
 }
 
 func (b *FfprobeBuilder) SetShowEntries(showEntries string) *FfprobeBuilder {
 	b.options.showEntries = showEntries
+	log.Printf("FfprobeBuilder: show entries set to %q", showEntries)
 	return b
 }
 
 func (b *FfprobeBuilder) SetOutputFormat(outputFormat string) *FfprobeBuilder {
 	b.options.outputFormat = outputFormat
+	log.Printf("FfprobeBuilder: output format set to %q", outputFormat)
 	return b
 }
 
 func (b *FfprobeBuilder) SetInputFilePath(inputFilePath string) *FfprobeBuilder {
 	b.options.inputFilePath = inputFilePath
+	log.Printf("FfprobeBuilder: input file path set to %q", inputFilePath)
 	return b
 }
 
 func (b *FfprobeBuilder) Build() []string {
 	args := []string{}
+	log.Printf("FfprobeBuilder: building args with options: %+v", b.options)
 	if b.options.logLevel != "" {
 		args = append(args, "-v", b.options.logLevel)
 	}
@@ -52,5 +59,6 @@ func (b *FfprobeBuilder) Build() []string {
 	if b.options.inputFilePath != "" {
 		args = append(args, b.options.inputFilePath)
 	}
+	log.Printf("FfprobeBuilder: built args: %v", args)
 	return args
 }
