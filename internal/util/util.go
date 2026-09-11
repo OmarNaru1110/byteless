b1b570f36c523452c5968687e72b9f676a1a1d0c
Naru <omarnaru2002@gmail.com>
2026-09-11 19:44:12 +0300

    feat: add convert bytes to MB and calculate bitrate utils

--- internal/util/util.go
+++ internal/util/util.go
@@ -3,3 +3,12 @@ package util
 func ConvertMBToBytes(mb int) int {
 	return mb * 1024 * 1024
 }
+
+func ConvertBytesToMB(bytes int) int {
+	return bytes / (1024 * 1024)
+}
+
+func CalculateBitrate(videoSizeBytes int, durationSeconds int, audioBitrate int) int {
+	bitrate := (ConvertBytesToMB(videoSizeBytes)*8192)/durationSeconds - audioBitrate
+	return bitrate
+}
