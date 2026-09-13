6f2e5090d1daeee2f357dbb7807daaa608f47577
Naru <omarnaru2002@gmail.com>
2026-09-13 10:36:28 +0300

    feat: add FullPath method to Video and output video

--- internal/domain/output_video.go
+++ internal/domain/output_video.go
@@ -1,9 +1,15 @@
 package domain
 
+import "path/filepath"
+
 type OutputVideo struct {
 	TargetSize            int // bytes
 	TargetSizeAfterMargin int // bytes
 	OutputPath            string
 	OutputName            string
 	ElapsedTime           int // seconds
+}
+
+func (v OutputVideo) FullPath() string {
+	return filepath.Join(v.OutputPath, v.OutputName)
 }
--- internal/domain/video.go
+++ internal/domain/video.go
@@ -1,9 +1,15 @@
 package domain
 
+import "path/filepath"
+
 type Video struct {
 	Path         string `json:"path"`
 	Name         string `json:"name"`
 	Size         int    `json:"size"`         // bytes
 	Duration     int    `json:"duration"`     // seconds
 	AudioBitrate int    `json:"audioBitrate"` // kbps
+}
+
+func (v Video) FullPath() string {
+	return filepath.Join(v.Path, v.Name)
 }
