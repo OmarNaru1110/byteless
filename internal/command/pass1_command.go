d34ab13f68b162f923b50901926d0371d7e0e263
Naru <omarnaru2002@gmail.com>
2026-09-13 10:24:16 +0300

    feat: build pass 1 command

--- internal/command/pass1_command.go
+++ internal/command/pass1_command.go
@@ -16,9 +16,15 @@ type TwoPassEncodePass1Command struct {
 	builder *builder.FfmpegBuilder
 }
 
-func NewTwoPassEncodePass1Command() *TwoPassEncodePass1Command {
+func NewTwoPassEncodePass1Command(inputPath string, videoBitrateKbps int) *TwoPassEncodePass1Command {
 	return &TwoPassEncodePass1Command{
-		builder: builder.NewFfmpegBuilder("ffmpeg"), //complete the command list,
+		builder: builder.NewFfmpegBuilder("ffmpeg").
+			SetInputFilePath(inputPath).
+			SetVideoCodec("libx264").
+			SetVideoBitrate(fmt.Sprintf("%dk", videoBitrateKbps)).
+			SetPass(1).
+			DisableAudio().
+			SetFormat("null"),
 	}
 }
 
