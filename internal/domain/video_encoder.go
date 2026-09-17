1135179a910b89bfa0b28a700c5695a8c58e8282
Naru <omarnaru2002@gmail.com>
2026-09-17 16:19:43 +0300

    refactor: refactor video encoder

--- internal/domain/video_encoder.go
+++ internal/domain/video_encoder.go
@@ -3,6 +3,21 @@ package domain
 type VideoEncoder string
 
 const (
-	H264 VideoEncoder = "H.264"
-	H265 VideoEncoder = "H.265"
+	H264 VideoEncoder = "libx264"
+	H265 VideoEncoder = "libx265"
 )
+
+type EncoderInfo struct {
+	ID          VideoEncoder `json:"id"`
+	Name        string       `json:"name"`
+	Description string       `json:"description"`
+}
+
+var availableEncoders = []EncoderInfo{
+	{ID: H264, Name: "H.264", Description: "Fast, widely compatible"},
+	{ID: H265, Name: "H.265", Description: "Better compression, slower"},
+}
+
+func GetEncoders() []EncoderInfo {
+	return availableEncoders
+}
